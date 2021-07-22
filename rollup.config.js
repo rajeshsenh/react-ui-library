15b7756c830d2693e4d5bf01634c7448fae7de4f
rajesh <rajesh.sen@customerxps.com>
2021-07-22 18:35:19 +0530

    add rollup and babel

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import { terser } from 'rollup-plugin-terser';
+
+const dist = 'dist';
+const production = !process.env.ROLLUP_WATCH;
+const bundle = "bundle";
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: `${dist}/${bundle}.cjs.js`,
+      format: 'cjs'
+    },
+    {
+      file: `${dist}/${bundle}.esm.js`,
+      format: 'esm'
+    },
+    {
+      name: "react-ui-library",
+      file: `${dist}/${bundle}.umd.js`,
+      globals: {
+        react: 'React'
+      },
+      format: 'umd'
+    }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    production && terser()
+  ],
+  external: ['react']
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("working" + 'fine');
+import React from "react";
+
+export const Spinner = props => <h1>Spinner</h1>;
